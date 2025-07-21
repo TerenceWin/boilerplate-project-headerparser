ab53c514953972c4317ab0349fe116fc63c45398
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-21 16:19:26 +0900

    First try: Project submition

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress:"req.ip", 
+    language:"Chinese",
+    software:"I3 core "
+  });
+}); 
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
